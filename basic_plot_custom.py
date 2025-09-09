5e6281584019f0adfaca82ca1762bd7e764a2e98
Abhishek Joshi <avjoshi2@illinois.edu>
2025-09-09 15:15:18 -0600

    added panel plots to custom reader

--- basic_plot_custom.py
+++ basic_plot_custom.py
@@ -57,6 +57,33 @@ def get_label_dictionary(variables):
       label_dict[variable] = variable
   return label_dict
 
+def generate_plot_kwargs_dict(kwargs):
+  # Extract possible per-variable plotting options
+  vmin_dict={}
+  vmax_dict={}
+  cmap_dict={}
+  for key in ['vmin', 'vmax', 'cmap']:
+    plot_dict = kwargs.pop(key, {})
+    if(plot_dict)!={}:
+      # if plot kwarg is a dictionary or a number
+      try:
+        plot_dict = eval(str(plot_dict))
+        if type(plot_dict) != dict:
+          val = plot_dict
+          plot_dict = {var: val for var in variables}
+      # if plot kwarg is a string
+      except NameError:
+        val = plot_dict
+        plot_dict = {var: val for var in variables}
+
+      if key == 'vmin':
+        vmin_dict = plot_dict
+      elif key == 'vmax':
+        vmax_dict = plot_dict
+      elif key == 'cmap':
+        cmap_dict = plot_dict
+  return vmin_dict,vmax_dict,cmap_dict
+
 def plot_slice(slice_data,slice_grid,header,ath_file='test',variables="rho", slice_dim=0,loc=0, remain_coord_range=None, length_scale=1, length_scale_label='',output_dir=None,**kwargs):
   """
   Plot a 2D slice of a 3D Athena++ HDF5 dataset using cell interface coordinates.
@@ -107,8 +134,10 @@ def plot_slice(slice_data,slice_grid,header,ath_file='test',variables="rho", sli
 
   # Get simulation time
   time = header['Time']
-
+  # Generate dictionaries of labels and plot kwargs
   label_dict = get_label_dictionary(variables)
+  vmin_dict,vmax_dict,cmap_dict = generate_plot_kwargs_dict(kwargs)
+
   for ind,variableName in enumerate(variables):
     plt.figure(num=pid,clear=True,figsize=(width,height))
     ax = plt.gca()
@@ -120,12 +149,27 @@ def plot_slice(slice_data,slice_grid,header,ath_file='test',variables="rho", sli
       variable = variableName
     var_ind = variables.index(variableName)
     print(f"{variable} max {np.max(slice_data[...,var_ind]):.2e}, min {np.min(slice_data[...,var_ind]):.2e}")
+
+    # Determine color scale and colormap for this variable
+    vmin = vmin_dict.get(variableName, None)
+    vmax = vmax_dict.get(variableName, None)
+    cmap = cmap_dict.get(variableName, None)
+
+    # Prepare pcolormesh kwargs for this variable
+    pcolormesh_kwargs = kwargs.copy()
+    if vmin is not None:
+      pcolormesh_kwargs['vmin'] = vmin
+    if vmax is not None:
+      pcolormesh_kwargs['vmax'] = vmax
+    if cmap is not None:
+      pcolormesh_kwargs['cmap'] = cmap
+
     for i in range(slice_grid.shape[0]):
         data_toplot=slice_data[i,...,var_ind]
         if(log_flag):
-          im=ax.pcolormesh(slice_grid[i,:,0]/length_scale,slice_grid[i,:,1]/length_scale,np.log10(abs(data_toplot)),**kwargs)
+          im=ax.pcolormesh(slice_grid[i,:,0]/length_scale,slice_grid[i,:,1]/length_scale,np.log10(abs(data_toplot)),**pcolormesh_kwargs)
         else:
-          im=ax.pcolormesh(slice_grid[i,:,0]/length_scale,slice_grid[i,:,1]/length_scale,data_toplot,**kwargs)
+          im=ax.pcolormesh(slice_grid[i,:,0]/length_scale,slice_grid[i,:,1]/length_scale,data_toplot,**pcolormesh_kwargs)
     
     slice_coord_dims = list(set(range(3))-{slice_dim})
     slice_coords_list = [f"x{i+1}" for i in slice_coord_dims]
@@ -151,13 +195,131 @@ def plot_slice(slice_data,slice_grid,header,ath_file='test',variables="rho", sli
     plt.savefig(plt_filename,bbox_inches="tight",dpi=200)
     plt.clf()
   plt.close()
-  
 
-def process_file(ath_file,plot_type,slice_kwargs,plot_kwargs):
+def plot_panel_slice(slice_data,slice_grid,header,ath_file='test',variables="rho", slice_dim=0,loc=0, remain_coord_range=None, length_scale=1, length_scale_label='',output_dir=None,**kwargs):
   """
-  Loads the slice and grid structures, and computes all the variables requested
+  Plot a 2D slice of a 3D Athena++ HDF5 dataset using cell interface coordinates.
+
+  Parameters:
+  -----------
+  slice_data : np.ndarray
+      2D slices of hydro variables from meshblocks intersecting the slice location.
+      Shape: (Nmb_selected, Nvars, mb, mb).
+  slice_grid : np.ndarray
+      Corresponding face-centered grid coordinates in the remaining two dimensions.
+      Shape: (Nmb_selected, mb+1, 2)  
+  ath_file: string
+      Name of the dump file for png name
+  variables : list
+      List of the variables to plot (e.g., "density", "pressure", etc.)
+      Should match Nvars from slice_data
+  slice_dim : int
+      Dimension to slice along: 0 (x1), 1 (x2), or 2 (x3).
+  loc : float
+      Physical location along `slice_dim` where the slice is taken.
+  remain_coord_range : float or None
+      Maximum value of the coordinates for the slice
+  length_scale : float
+      Length scale in code unites to divide the axes values with
+  length_scale_label : string
+      String for axes labels
+  **kwargs : dict
+      Additional keyword arguments passed to matplotlib.pyplot.pcolormesh
   """
-  variables=plot_kwargs['variables']
+
+  
+  pid = os.getpid()
+
+  grid_min = np.min(slice_grid,axis=(0,1))
+  grid_max = np.max(slice_grid,axis=(0,1))
+
+  xrange = grid_max[0]-grid_min[0]
+  yrange = grid_max[1]-grid_min[1]
+
+  # aspect ratio of each subplot
+  #aspect_ratio = max(4/9,min(9/4,xrange/yrange))
+  aspect_ratio = xrange/yrange
+  if(aspect_ratio<=1):
+    width = 4
+    height = width/aspect_ratio
+  else:
+    height = 4
+    width = height * aspect_ratio
+
+  n_vars = len(variables)
+  nrows = max(n_vars//4,1)
+  ncols = min(n_vars,4)
+  fig, axes = plt.subplots(nrows, ncols, figsize=(1.2*(ncols)*width, nrows*height), sharey=True, sharex=True, squeeze=False)
+
+  # Get simulation time
+  time = header['Time']
+  # Generate dictionaries of labels and plot kwargs
+  label_dict = get_label_dictionary(variables)
+  vmin_dict,vmax_dict,cmap_dict = generate_plot_kwargs_dict(kwargs)
+
+  for ind,variableName in enumerate(variables):
+    ax = axes.flatten()[ind]
+    if ('log' in variableName):
+      variable = variableName.replace('log','')
+      log_flag = True
+    else:
+      log_flag = False
+      variable = variableName
+    var_ind = variables.index(variableName)
+    print(f"{variable} max {np.max(slice_data[...,var_ind]):.2e}, min {np.min(slice_data[...,var_ind]):.2e}")
+
+    # Determine color scale and colormap for this variable
+    vmin = vmin_dict.get(variableName, None)
+    vmax = vmax_dict.get(variableName, None)
+    cmap = cmap_dict.get(variableName, None)
+
+    # Prepare pcolormesh kwargs for this variable
+    pcolormesh_kwargs = kwargs.copy()
+    if vmin is not None:
+      pcolormesh_kwargs['vmin'] = vmin
+    if vmax is not None:
+      pcolormesh_kwargs['vmax'] = vmax
+    if cmap is not None:
+      pcolormesh_kwargs['cmap'] = cmap
+
+    for i in range(slice_grid.shape[0]):
+        data_toplot=slice_data[i,...,var_ind]
+        if(log_flag):
+          im=ax.pcolormesh(slice_grid[i,:,0]/length_scale,slice_grid[i,:,1]/length_scale,np.log10(abs(data_toplot)),**pcolormesh_kwargs)
+        else:
+          im=ax.pcolormesh(slice_grid[i,:,0]/length_scale,slice_grid[i,:,1]/length_scale,data_toplot,**pcolormesh_kwargs)
+    
+    slice_coord_dims = list(set(range(3))-{slice_dim})
+    slice_coords_list = [f"x{i+1}" for i in slice_coord_dims]
+
+    if(ind//4==nrows-1):
+      ax.set_xlabel(f"{slice_coords_list[0]}{length_scale_label}")
+    if(ind%4==0):
+      ax.set_ylabel(f"{slice_coords_list[1]}{length_scale_label}")
+    ax.set_title(f"{label_dict[variables[ind]]}")
+    cbar = fig.colorbar(im,ax=ax,pad=0)
+
+    remaining_coord_field=f"x{slice_dim+1}"
+    ax.set_xlim(np.array(remain_coord_range[:2])/length_scale)
+    ax.set_ylim(np.array(remain_coord_range[2:])/length_scale)
+  
+  plt.suptitle(f"Panel at t={time:.2f}",y=(1.0+0.02*ncols))
+  plt.subplots_adjust(left=0,right=1,bottom=0,top=1)
+
+  variableNames = "-".join(variables)
+  if(remain_coord_range==None):
+    plt_filename = f"{os.path.splitext(ath_file)[0]}_slice_{variableNames}_{str.join('',slice_coords_list)}_{remaining_coord_field}_{loc}.png"
+  else:
+    plt_filename = f"{os.path.splitext(ath_file)[0]}_slice_{variableNames}_{str.join('',slice_coords_list)}_{np.max(remain_coord_range):.1e}_{remaining_coord_field}_{loc}.png"
+  
+  if output_dir!=None:
+    plt_filename = os.path.join(output_dir,os.path.basename(plt_filename))
+  
+  plt.savefig(plt_filename,bbox_inches="tight",dpi=200)
+  plt.clf()
+  plt.close()
+  
+def load_slice_and_variables(ath_file,variables,slice_kwargs):
   hfp = h5py.File(ath_file)
   hydro = hfp['hydro']
   xvs = [hfp['x1v'],hfp['x2v'],hfp['x3v']]
@@ -197,21 +359,28 @@ def process_file(ath_file,plot_type,slice_kwargs,plot_kwargs):
         slice_data[...,ind]=slice_hydro[...,press_ind]/(np.sqrt(slice_data[...,ind])/2)
       # isothermal
       except Exception as e:
-        if ('cs' in plot_kwargs):
-          cs = plot_kwargs['cs']
+        if ('cs' in slice_kwargs.keys()):
+          cs = slice_kwargs['cs']
         else:
           cs = 0.05
         rho_ind = var_list_full.index('rho')
         slice_data[...,ind]=cs**2 * slice_hydro[...,rho_ind]/(np.sqrt(slice_data[...,ind])/2)
     else:
       print(f"processing {variable} has not yet been implemented! skipping")
-  
+  return slice_data,slice_grid,header
+
+def process_file(ath_file,plot_type,slice_kwargs,plot_kwargs):
+  """
+  Loads the slice and grid structures, and computes all the variables requested
+  """
+
+  slice_data,slice_grid,header=load_slice_and_variables(ath_file,plot_kwargs['variables'],slice_kwargs)  
 
   print(f"Processed {ath_file}")
   if(plot_type=='slice'):
     plot_slice(slice_data,slice_grid,header,ath_file,**plot_kwargs)
-  # # elif(plot_type=='panel'):
-  # #   plot_panel_slice(ath_data, ath_file=ath_file, **plot_kwargs)
+  elif(plot_type=='panel'):
+    plot_panel_slice(slice_data,slice_grid,header,ath_file,**plot_kwargs)
   # # elif(plot_type=='project'):
   # #   print("still need to write projection plot code")  
   print(f"Plotted: {ath_file}")
@@ -260,9 +429,6 @@ def load_and_plot(files_and_kwargs,plot_type,num_cores_multiplier,parallel,varia
   if(slice_dim not in set(range(3))):
     print(f"Specify a valid axis to slice! slice_dim={slice_dim} valid numbers: x1=0, x2=1, x3=2")
     exit()
-  # modify slice_coords to be a list
-  slice_coord_dims = list(set(range(3))-{slice_dim})
-  slice_coords_list = ["x{i+1}" for i in slice_coord_dims]
 
   # set the slice bounds if specified
   if (slice_domain != None):
